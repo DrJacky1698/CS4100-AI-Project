975d2802a5e1924a642ab45c0bae0dfbeac02855
DrJacky1698 <Yw020416@gmail.com>
2023-12-13 16:59:56 -0500

    update FENtoEvaluationValues.py

--- Util/FENtoEvaluationValues.py
+++ Util/FENtoEvaluationValues.py
@@ -1,6 +1,6 @@
 import csv
 import chess
-from Algorithm.EvaluationFunctions import relative_basic_piece_squares_and_material_evaluator, stockfish_evaluator, initializeStockfishEngine, closeStockfishEngine, relative_tapered_piece_squares_evaluator, relative_king_safety_evaluator, relative_pieces_attacked_evaluator, relative_simple_piece_count_evaluator
+from Algorithm.EvaluationFunctions import relative_simple_material_evaluator, stockfish_evaluator, initializeStockfishEngine, closeStockfishEngine, relative_tapered_piece_squares_evaluator, relative_king_safety_evaluator, relative_pieces_attacked_evaluator, relative_simple_piece_count_evaluator
 
 
 def process_fen_data(input_csv, output_csv, batch_size=100):
@@ -10,11 +10,12 @@ def process_fen_data(input_csv, output_csv, batch_size=100):
         fen_data = [(row[0], row[1]) for row in reader] # as FEN is second column in our input csv file
 
     batch = []
+    processed_games = 0
 
     with open(output_csv, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Game Number", "FEN",
-                         "Relative_basic_piece_squares_and_material_evaluator (White)", 
+                         "Relative_simple_material_evaluator (White)", 
                          "Relative_tapered_piece_squares_evaluator (White)",
                          "Relative_king_safety_evaluator (White)",
                          "Relative_simple_piece_count_evaluator (White)",
@@ -25,7 +26,7 @@ def process_fen_data(input_csv, output_csv, batch_size=100):
             board = chess.Board(fen)
             evaluations = [game_number, fen]
             
-            evaluations.append(relative_basic_piece_squares_and_material_evaluator(board, 'white'))
+            evaluations.append(relative_simple_material_evaluator(board, 'white'))
             evaluations.append(relative_tapered_piece_squares_evaluator(board, 'white'))
             evaluations.append(relative_king_safety_evaluator(board, 'white'))
             evaluations.append(relative_simple_piece_count_evaluator(board, 'white'))
@@ -33,6 +34,10 @@ def process_fen_data(input_csv, output_csv, batch_size=100):
             evaluations.append(stockfish_evaluator(board, 'white'))
 
             batch.append(evaluations)
+            processed_games += 1
+
+            if processed_games % batch_size == 0 or processed_games == len(fen_data):
+                print(f"Processed {processed_games} games so far.")
 
             if len(batch) >= batch_size:
                 writer.writerows(batch)
