c1a64d0352ac5677ab8e505997faec70a8d48e0d
DrJacky1698 <Yw020416@gmail.com>
2023-12-13 16:43:49 -0500

    Update FENtoEvaluationValues.py

--- Util/FENtoEvaluationValues.py
+++ Util/FENtoEvaluationValues.py
@@ -1,6 +1,6 @@
 import csv
 import chess
-from Algorithm.EvaluationFunctions import basic_piece_squares_and_material_evaluator, stockfish_evaluator, initializeStockfishEngine, closeStockfishEngine
+from Algorithm.EvaluationFunctions import relative_basic_piece_squares_and_material_evaluator, stockfish_evaluator, initializeStockfishEngine, closeStockfishEngine, relative_simple_material_evaluator, relative_tapered_piece_squares_evaluator, relative_king_safety_evaluator, relative_pieces_attacked_evaluator, relative_simple_piece_count_evaluator
 
 
 def process_fen_data(input_csv, output_csv, batch_size=100):
@@ -13,17 +13,25 @@ def process_fen_data(input_csv, output_csv, batch_size=100):
 
     with open(output_csv, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Game Number", "FEN", "Simple Material Evaluator (White)", "Simple Material Evaluator (Black)", 
-                         "Stockfish Evaluator (White)", "Stockfish Evaluator (Black)"])
+        writer.writerow(["Game Number", "FEN", "Relative_simple_material_evaluator",
+                         "Relative_basic_piece_squares_and_material_evaluator (White)", 
+                         "Relative_tapered_piece_squares_evaluator (White)",
+                         "Relative_king_safety_evaluator (White)",
+                         "Relative_simple_piece_count_evaluator (White)",
+                         "Relative_pieces_attacked_evaluator (White)",
+                         "Stockfish Evaluator (White)"])
 
         for game_number, fen in fen_data:
             board = chess.Board(fen)
             evaluations = [game_number, fen]
             
-            evaluations.append(basic_piece_squares_and_material_evaluator(board, 'white'))
-            # evaluations.append(basic_piece_squares_and_material_evaluator(board, 'black'))
+            evaluations.append(relative_simple_material_evaluator(board, 'white'))
+            evaluations.append(relative_basic_piece_squares_and_material_evaluator(board, 'white'))
+            evaluations.append(relative_tapered_piece_squares_evaluator(board, 'white'))
+            evaluations.append(relative_king_safety_evaluator(board, 'white'))
+            evaluations.append(relative_simple_piece_count_evaluator(board, 'white'))
+            evaluations.append(relative_pieces_attacked_evaluator(board, 'white'))
             evaluations.append(stockfish_evaluator(board, 'white'))
-            # evaluations.append(stockfish_evaluator(board, 'black'))
 
             batch.append(evaluations)
 
