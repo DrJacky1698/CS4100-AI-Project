ae4d67bf0855d44297439d87c0a45cf08847ef30
DrJacky1698 <Yw020416@gmail.com>
2023-12-14 06:40:43 -0500

    update random forest algorithm

--- Algorithm/RandomForest.py
+++ Algorithm/RandomForest.py
@@ -14,29 +14,29 @@ import matplotlib.pyplot as plt
 # learning algorithms. This is particularly useful for handling high-dimensional data with many unique features. "
 # and there is indeed some other approach such as "vectorization", which tokenizes strings and counting 
 # the occurrences of these tokens in each document. But we are not using it.
-def extract_features_from_fen(fen):
+# def extract_features_from_fen(fen):
     # split the FEN string into its components
-    parts = fen.split(' ')
-    piece_placement = parts[0]
+    # parts = fen.split(' ')
+    # piece_placement = parts[0]
 
-    # initialize a dictionary to hold the features
-    features = {
-        'white_pawn': 0, 'black_pawn': 0, 'white_knight': 0, 'black_knight': 0,
-        'white_bishop': 0, 'black_bishop': 0, 'white_rook': 0, 'black_rook': 0,
-        'white_queen': 0, 'black_queen': 0, 'white_king': 0, 'black_king': 0
-    }
+    # # initialize a dictionary to hold the features
+    # features = {
+    #     'white_pawn': 0, 'black_pawn': 0, 'white_knight': 0, 'black_knight': 0,
+    #     'white_bishop': 0, 'black_bishop': 0, 'white_rook': 0, 'black_rook': 0,
+    #     'white_queen': 0, 'black_queen': 0, 'white_king': 0, 'black_king': 0
+    # }
 
-    # mapping of pieces to features
-    piece_to_feature = {
-        'P': 'white_pawn', 'p': 'black_pawn', 'N': 'white_knight', 'n': 'black_knight',
-        'B': 'white_bishop', 'b': 'black_bishop', 'R': 'white_rook', 'r': 'black_rook',
-        'Q': 'white_queen', 'q': 'black_queen', 'K': 'white_king', 'k': 'black_king'
-    }
+    # # mapping of pieces to features
+    # piece_to_feature = {
+    #     'P': 'white_pawn', 'p': 'black_pawn', 'N': 'white_knight', 'n': 'black_knight',
+    #     'B': 'white_bishop', 'b': 'black_bishop', 'R': 'white_rook', 'r': 'black_rook',
+    #     'Q': 'white_queen', 'q': 'black_queen', 'K': 'white_king', 'k': 'black_king'
+    # }
 
     # count each piece
-    for char in piece_placement:
-        if char in piece_to_feature:
-            features[piece_to_feature[char]] += 1
+    # for char in piece_placement:
+    #     if char in piece_to_feature:
+    #         features[piece_to_feature[char]] += 1
 
     # at this point, actually we can also extract more useful information to be included
     # such as whose turn it is, castling rights and so on
@@ -44,57 +44,67 @@ def extract_features_from_fen(fen):
     # example could be features['turn'] = 1 if parts[1] == 'w' else 0 for determining the turn
     # in case we are all training from white side so never mind
 
-    return features
+    # return features
 
-def feature_hashing(df, column):
-    hasher = FeatureHasher(input_type='dict')
-    hashed_features = hasher.transform(df[column].apply(extract_features_from_fen))
-    return hashed_features
+# def feature_hashing(df, column):
+#     hasher = FeatureHasher(input_type='dict')
+#     hashed_features = hasher.transform(df[column].apply(extract_features_from_fen))
+#     return hashed_features
 
-def process_and_save_chunk(chunk, output_file):
-    hashed_features = feature_hashing(chunk, 'FEN')
-    hashed_features_df = pd.DataFrame(hashed_features.toarray())
-    chunk_processed = pd.concat([hashed_features_df, chunk], axis=1).drop('FEN', axis=1)
+# def process_and_save_chunk(chunk, output_file):
+#     hashed_features = feature_hashing(chunk, 'FEN')
+#     hashed_features_df = pd.DataFrame(hashed_features.toarray())
+#     chunk_processed = pd.concat([hashed_features_df, chunk], axis=1).drop('FEN', axis=1)
 
     # Save processed chunk
-    chunk_processed.to_csv(output_file, mode='a', header=not os.path.exists(output_file), index=False)
+    # chunk_processed.to_csv(output_file, mode='a', header=not os.path.exists(output_file), index=False)
 
-chunk_size = 1000
-input_file_path = 'Algorithm/evaluation_results_dimitar.csv'
-processed_file_path = 'Algorithm/processed_evaluation_results.csv'
+# chunk_size = 1000
+# input_file_path = 'Algorithm/evaluation_results_dimitar.csv'
+# processed_file_path = 'Algorithm/processed_evaluation_results.csv'
 
-if not os.path.exists(processed_file_path):
-    print("Processed file not found. Starting feature extraction and processing...")
-    chunk_reader = pd.read_csv(input_file_path, chunksize=chunk_size)
+# if not os.path.exists(processed_file_path):
+#     print("Processed file not found. Starting feature extraction and processing...")
+#     chunk_reader = pd.read_csv(input_file_path, chunksize=chunk_size)
 
-    for i, chunk in enumerate(chunk_reader):
-        print(f"Processing chunk {i + 1}...")
-        process_and_save_chunk(chunk, processed_file_path)
-        print(f"{(i + 1) * chunk_size} rows processed and saved.")
+#     for i, chunk in enumerate(chunk_reader):
+#         print(f"Processing chunk {i + 1}...")
+#         process_and_save_chunk(chunk, processed_file_path)
+#         print(f"{(i + 1) * chunk_size} rows processed and saved.")
 
-else:
-    print("Loading processed data from the CSV file...")
+# else:
+#     print("Loading processed data from the CSV file...")
+
+# above is some try but did not make sense any more, it would be too time-comsuming
 
 # reading and running the test model
 # there is no tunning tecnique used so it is just a test
 # to see how long it would take to simply run a regressor
 print("Reading data...")
-df = pd.read_csv('Algorithm/processed_evaluation_results.csv')
+df = pd.read_csv('Algorithm/evaluation_results_dimitar.csv')
 
-print("Extracting features using feature hashing...")
-hashed_features = feature_hashing(df, 'FEN')
+# print("Extracting features using feature hashing...")
+# hashed_features = feature_hashing(df, 'FEN')
 
 # add hashed features to original data
-hashed_features_df = pd.DataFrame(hashed_features.toarray())
-df = pd.concat([hashed_features_df, df], axis=1)
+# hashed_features_df = pd.DataFrame(hashed_features.toarray())
+# df = pd.concat([hashed_features_df, df], axis=1)
+# df = df.drop('FEN', axis=1)
+
 df = df.drop('FEN', axis=1)
+
+# Convert 'Stockfish Evaluator (White)' column to numeric, including handling of + and - signs
+df['Stockfish Evaluator (White)'] = pd.to_numeric(df['Stockfish Evaluator (White)'], errors='coerce')
+print("Number of NaN values:", df['Stockfish Evaluator (White)'].isna().sum())
+df = df.dropna(subset=['Stockfish Evaluator (White)'])
+
 X = df.drop('Stockfish Evaluator (White)', axis=1)
 Y = df['Stockfish Evaluator (White)']
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 print("Start training Random Forest Regressor...")
-regressor = RandomForestRegressor(random_state=42)
+regressor = RandomForestRegressor(random_state=0)
 regressor.fit(X_train, y_train)
 
 
