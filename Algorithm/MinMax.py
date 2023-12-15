4beeee2254ad25b7b2be75275f58e480cdefbfa5
DrJacky1698 <Yw020416@gmail.com>
2023-12-14 20:41:02 -0500

    update MinMax to relect the player color

--- Algorithm/MinMax.py
+++ Algorithm/MinMax.py
@@ -1,26 +1,28 @@
 import chess
 import chess.engine
-from Algorithm.EvaluationFunctions import simple_material_evaluator
 from copy import deepcopy
+
+import chess
 
 class MinMax:
     def __init__(self, evaluation_function):
         self.evaluation_function = evaluation_function
 
-    def min_max(self, board, depth):
+    def min_max(self, board, depth, player_color):
         if depth == 0 or board.is_game_over():
-            return self.evaluation_function(board), None
+            return self.evaluation_function(board, player_color), None
 
         moves = list(board.legal_moves)
-        best_score = float('-inf') if board.turn else float('inf')
+        best_score = float('-inf') if board.turn == chess.WHITE else float('inf')
         best_move = None
 
         for move in moves:
             temp_board = deepcopy(board)
             temp_board.push(move)
-            score, _ = self.min_max(temp_board, depth - 1)
+            next_player_color = "black" if player_color == "white" else "white"
+            score, _ = self.min_max(temp_board, depth - 1, next_player_color)
 
-            if board.turn:
+            if board.turn == chess.WHITE:
                 if score > best_score:
                     best_score = score
                     best_move = move
@@ -32,9 +34,10 @@ class MinMax:
         return best_score, best_move
 
     def select_move(self, board, depth=3):
-        _, best_move = self.min_max(board, depth)
+        player_color = "white" if board.turn == chess.WHITE else "black"
+        _, best_move = self.min_max(board, depth, player_color)
         return best_move
 
-def play_min_max(board):
-    minmax = MinMax(simple_material_evaluator)
+def play_min_max(board, evaluator):
+    minmax = MinMax(evaluator)
     return minmax.select_move(board)
